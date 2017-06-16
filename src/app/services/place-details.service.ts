a8d544e9613467a6404396bf46618eb2c96dfbc3
Dominik <dongron@wp.pl>
2017-06-17 01:18:50 +0200

    Added get place by id and update placeholder

--- src/app/services/place-details.service.ts
+++ src/app/services/place-details.service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import {Place} from '../models/place';
 
 @Injectable()
 export class PlaceDetailsService {
 
-  constructor() { }
+  url = 'https://places-back-end.herokuapp.com/api/places/';
+
+  constructor(private http: Http) {
+  }
+
+  getPlaceDetails(id: string) {
+    const urlString = this.url + 'byId/' + id;
+    console.log(urlString);
+    return this.http.get(urlString);
+  }
+
+  updatePlace(newPlace: Place) {
+    
+  }
 
 }
