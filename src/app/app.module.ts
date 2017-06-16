cc6cf8d623e42359e2200bb4859956469b4e7658
Dominik <dongron@wp.pl>
2017-06-16 13:34:22 +0200

    Routing configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,54 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core'
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './componenets/login/login.component';
-import { PlacesDetailsComponent } from './componenets/places-details/places-details.component';
-import { PlacesListComponent } from './componenets/places-list/places-list.component';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './componenets/login/login.component';
+import {PlacesDetailsComponent} from './componenets/places-details/places-details.component';
+import {PlacesListComponent} from './componenets/places-list/places-list.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule, MdToolbarModule, MdButtonModule, MdInputModule} from '@angular/material';
+import { NotFoundComponent } from './componenets/not-found/not-found.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: {title: 'Log in'}
+  },
+  {
+    path: 'places',
+    component: PlacesListComponent,
+    data: {title: 'Places'}
+  },
+  {
+    path: 'details',
+    component: PlacesDetailsComponent,
+    data: {title: 'Place details'}
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     PlacesDetailsComponent,
-    PlacesListComponent
+    PlacesListComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     BrowserAnimationsModule,
     MaterialModule,
     MdToolbarModule,
@@ -26,4 +58,5 @@ import {MaterialModule, MdToolbarModule, MdButtonModule, MdInputModule} from '@a
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
