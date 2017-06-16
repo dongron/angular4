e21762ece089333e7dd1b4f32e90f7847143531a
Dominik <dongron@wp.pl>
2017-06-16 22:04:28 +0200

    Places service with methods created

--- src/app/services/places.service.ts
+++ src/app/services/places.service.ts
@@ -1,8 +1,37 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import {Place} from '../models/place';
+import {User} from '../models/user';
 
 @Injectable()
 export class PlacesService {
 
-  constructor() { }
+  url = 'https://places-back-end.herokuapp.com/api/places/';
+
+  constructor(private http: Http) {
+  }
+
+  getAllPlaces() {
+    const urlString = this.url + 'all';
+    console.log(urlString);
+    return this.http.get(urlString);
+  }
+
+  getUsersPlaces(userEmail: string) {
+    const urlString = this.url + 'all';
+    console.log(urlString);
+    this.http.get(urlString).subscribe(
+      res => {
+        const places: Place[] = res.json();
+        let userPlaces: Place[] = [];
+        for (const place of places) {
+          if (place.ownerEmail === userEmail) {
+            userPlaces.push(place);
+          }
+        }
+        return userPlaces;
+      }
+    );
+  }
 
 }
