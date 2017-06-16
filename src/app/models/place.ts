3976040c688aa99fdfe7ad633f6980de040ee98d
Dominik <dongron@wp.pl>
2017-06-16 22:20:46 +0200

    Added getters and setters for models

--- src/app/models/place.ts
+++ src/app/models/place.ts
@@ -1,10 +1,73 @@
 export class Place {
-  _id;
-  _v;
-  name;
-  street;
-  streetNr;
-  city;
-  geo;
-  ownerEmail;
+  private _id;
+  private _v;
+  private _name;
+  private _street;
+  private _streetNr;
+  private _city;
+  private _geo;
+  private _ownerEmail;
+
+  get id() {
+    return this._id;
+  }
+
+  set id(value) {
+    this._id = value;
+  }
+
+  get v() {
+    return this._v;
+  }
+
+  set v(value) {
+    this._v = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get street() {
+    return this._street;
+  }
+
+  set street(value) {
+    this._street = value;
+  }
+
+  get streetNr() {
+    return this._streetNr;
+  }
+
+  set streetNr(value) {
+    this._streetNr = value;
+  }
+
+  get city() {
+    return this._city;
+  }
+
+  set city(value) {
+    this._city = value;
+  }
+
+  get geo() {
+    return this._geo;
+  }
+
+  set geo(value) {
+    this._geo = value;
+  }
+
+  get ownerEmail() {
+    return this._ownerEmail;
+  }
+  set ownerEmail(value) {
+    this._ownerEmail = value;
+  }
 }
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,8 +1,55 @@
 export class User {
-  _id;
-  _v;
-  username;
-  email;
-  password;
-  city;
+  private _id;
+  private _v;
+  private _username;
+  private _email;
+  private _password;
+  private _city;
+
+  get id() {
+    return this._id;
+  }
+
+  set id(value) {
+    this._id = value;
+  }
+
+  get v() {
+    return this._v;
+  }
+
+  set v(value) {
+    this._v = value;
+  }
+
+  get username() {
+    return this._username;
+  }
+
+  set username(value) {
+    this._username = value;
+  }
+
+  get email() {
+    return this._email;
+  }
+
+  set email(value) {
+    this._email = value;
+  }
+
+  get password() {
+    return this._password;
+  }
+
+  set password(value) {
+    this._password = value;
+  }
+
+  get city() {
+    return this._city;
+  }
+  set city(value) {
+    this._city = value;
+  }
 }
