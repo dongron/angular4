e92c60588731972080916f68264691584d7f6b01
Dominik <dongron@wp.pl>
2017-06-18 14:20:50 +0200

    Reservations service with few working methods

--- src/app/services/reservations.service.ts
+++ src/app/services/reservations.service.ts
@@ -1,8 +1,36 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import {Place} from '../models/place';
+import {User} from '../models/user';
 
 @Injectable()
 export class ReservationsService {
 
-  constructor() { }
+  url = 'https://places-back-end.herokuapp.com/api/reservations/';
+
+  constructor(private http: Http) {
+  }
+
+  isReservationForPlace(reservation, placeName) {
+    return reservation.placeName === placeName;
+  }
+
+  getAllReservations() {
+    const urlString = this.url + 'all';
+    console.log(urlString);
+    return this.http.get(urlString);
+  }
+
+  getReservationsByPlaceName(placeName) {
+    return this.getAllReservations().filter(this.isReservationForPlace.bind(this, placeName));
+  }
+
+  addReservation() {
+
+  }
+
+  removeReservation() {
+
+  }
 
 }
