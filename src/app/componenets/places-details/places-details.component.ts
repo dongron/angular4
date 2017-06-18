18097152102562b3e8035d4017e5ba00989ab943
Dominik <dongron@wp.pl>
2017-06-18 14:21:13 +0200

    Reservations works for all of them

--- src/app/componenets/places-details/places-details.component.ts
+++ src/app/componenets/places-details/places-details.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Params, Router} from '@angular/router';
 import {Place} from '../../models/place';
+import {PlacesService} from '../../services/places.service';
+import {ReservationsService} from '../../services/reservations.service';
 
 @Component({
   selector: 'app-places-details',
@@ -9,16 +11,40 @@ import {Place} from '../../models/place';
 })
 export class PlacesDetailsComponent implements OnInit {
 
-  placeObj;
+  place: Place;
+  reservations;
 
-  constructor(router: Router, private activatedRoute: ActivatedRoute) {
+  constructor(router: Router, private activatedRoute: ActivatedRoute,
+              private placesService: PlacesService, private reservationsService: ReservationsService) {
   }
 
   ngOnInit() {
-    this.activatedRoute.params.subscribe((params: Params) => {
-      const placeId = params['id'];
-      console.log(placeId);
-    });
+    this.place = new Place();
+    this.activatedRoute.params.subscribe(
+      (params: Params) => {
+        const placeId = params['id'];
+        console.log(placeId);
+        this.placesService.getPlaceDetails(placeId).subscribe(
+          data => {
+            this.place = data.json();
+          }
+        );
+      });
+
+    this.reservationsService.getAllReservations().subscribe(
+      reservations => {
+        // this.reservations = reservations.json();
+        const results = Object.keys(reservations.json()).map(
+          function (e) {
+            return reservations.json()[e];
+          });
+        for (const result of results) {
+          result.time = new Date(result.time);
+        }
+        this.reservations = results;
+      }
+    );
+
   }
 
 }
